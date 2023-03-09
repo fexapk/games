5880935f31780072affc14abc17e240f3b95e362
Spike <acsiidax@protonmail.com>
2023-03-09 17:54:12 +0100

    finished tests & fiexed free price tag in csv data

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ package src;
 import src.models.*;
 import src.io.*;
 import java.util.Scanner;
-import java.util.concurrent.SynchronousQueue;
 
 public class Main {
     public static void main(String[] args) {
